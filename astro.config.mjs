a522c54db0282872e7d5dc30e1f2cb1943a990f7
Hoai Phong <phongduong1710@gmail.com>
2023-10-15 00:51:06 +0700

    update content and add cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +12,45 @@ export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        collections: [
+          {
+            name: "blog",
+            label: "Blog",
+            folder: "src/content/blog",
+            create: true,
+            delete: true,
+            fields: [
+              { name: "title", widget: "string", label: "Title" },
+              {
+                name: "date",
+                widget: "datetime",
+                label: "Date",
+                date_format: "YYYY-MM-DD",
+              },
+              { name: "body", widget: "markdown", label: "Body" },
+            ],
+          },
+          {
+            label: "Pages",
+            name: "pages",
+            files: [
+              {
+                label: "Home",
+                name: "home",
+                file: "src/content/pages/home.md",
+                fields: [{ label: "Body", name: "body", widget: "markdown" }],
+              },
+            ],
+          },
+        ],
+      },
     }),
   ],
 });
--- src/content/pages/home.md
+++ src/content/pages/home.md
@@ -1,7 +1,20 @@
-# Minimal blog theme
+# 👋 Hi
 
-A minimal theme for blog, created by [Hoai Phong](https://github.com/phongever) with:
+I am Phong, a developer trying to become an author
 
-- Astro
-- Tailwind CSS
-- Markdown
+You can contact me at [phongduong1710@gmail.com](mailto:phongduong1710@gmail.com)
+
+Or find me on
+
+- [Github](https://www.github.com/phongever)
+- [Instagram](https://www.instagram.com/phongever)
+- [Youtube](https://www.youtube.com/@phongever)
+- [LinkedIn](https://www.linkedin.com/in/phongever/)
+
+Here are my projects
+
+- [Koogio](https://play.google.com/store/apps/developer?id=Koogio)
+- [VN.css](https://github.com/phongever/VN.css)
+- [Minimal blog theme](https://github.com/phongever/minimal-blog-theme)
+
+And this is my [CV](https://docs.google.com/document/d/1NdmG3qd7fLs2oJQLouTGwcfjkg2q1_Yd0r3vLOTh82g)
